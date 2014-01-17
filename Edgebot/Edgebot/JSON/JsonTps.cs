990fe321065567529f347ac9610ae2023046ecf7
helkarakse <nimcuron@gmail.com>
2014-01-18 05:09:33 +0800

    Added new deserialization class for tps

---
+++ Edgebot/Edgebot/JSON/JsonTps.cs
@@ -0,0 +1,10 @@
+﻿namespace Edgebot.JSON
+{
+    public class JsonTps
+    {
+        public string Server { get; set; }
+        public string Tps { get; set; }
+        public int Count { get; set; }
+        public string Name { get; set; }
+    }
+}
