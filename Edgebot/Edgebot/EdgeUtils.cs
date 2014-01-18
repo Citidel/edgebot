f1a361ca9d415b9d4dc1df32a632742047ee3079
helkarakse <nimcuron@gmail.com>
2014-01-19 04:47:14 +0800

    Added xml documentation for EdgeUtils

--- Edgebot/Edgebot/EdgeUtils.cs
+++ Edgebot/Edgebot/EdgeUtils.cs
@@ -4,8 +4,17 @@ using ChatSharp;
 
 namespace Edgebot
 {
+    /// <summary>
+    /// Utility methods for EdgeBot
+    /// </summary>
     public class EdgeUtils
     {
+        /// <summary>
+        /// Sends a notice to a set of destinations
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="message"></param>
+        /// <param name="destinations"></param>
         public static void SendNotice(IrcClient client, string message, params string[] destinations)
         {
             const string illegalCharacters = "\r\n\0";
@@ -15,6 +24,12 @@ namespace Edgebot
             client.SendRawMessage("NOTICE {0} :{1}", to, message);
         }
 
+        /// <summary>
+        /// Sends a message to the channel
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="message"></param>
+        /// <exception cref="ArgumentException"></exception>
         public static void SendChannel(IrcClient client, string message)
         {
             const string illegalCharacters = "\r\n\0";
@@ -22,26 +37,53 @@ namespace Edgebot
             client.SendRawMessage("PRIVMSG {0} :{1}", EdgeData.Channel, message);
         }
 
+        /// <summary>
+        /// Returns true if the nickname is a developer
+        /// </summary>
+        /// <param name="nickname"></param>
+        /// <returns></returns>
         public static bool IsDev(string nickname)
         {
             return EdgeData.Developers.Any(str => str.Equals(nickname));
         }
 
+        /// <summary>
+        /// Returns true if the nickname is an operator
+        /// </summary>
+        /// <param name="client"></param>
+        /// <param name="nickname"></param>
+        /// <returns></returns>
         public static bool IsOp(IrcClient client, string nickname)
         {
             return client.Channels.Select(channel => channel.UsersByMode['o']).Any(users => users.Contains(nickname));
         }
 
+        /// <summary>
+        /// Returns a string formatted with the given style code
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <param name="colorCode"></param>
+        /// <returns></returns>
         public static string FormatText(object inputString, char colorCode)
         {
             return String.Format("{0}" + inputString + "{1}", colorCode, EdgeColors.Normal);
         }
 
+        /// <summary>
+        /// Returns a string formatted with the given color code
+        /// </summary>
+        /// <param name="inputString"></param>
+        /// <param name="formatCode"></param>
+        /// <returns></returns>
         public static string FormatColor(object inputString, string formatCode)
         {
             return String.Format("{0}" + inputString + "{1}", formatCode, EdgeColors.Normal);
         }
 
+        /// <summary>
+        /// Exception handler for the connection class
+        /// </summary>
+        /// <param name="exception"></param>
         public static void HandleException(Exception exception)
         {
             if (exception != null)
@@ -50,6 +92,11 @@ namespace Edgebot
             }
         }
 
+        /// <summary>
+        /// Logs a message in the console window and to the debug window
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="args"></param>
         public static void Log(string message, params object[] args)
         {
             if (!Program.Debug) return;
@@ -58,6 +105,10 @@ namespace Edgebot
             System.Diagnostics.Debug.Write(String.Format(message, args) + "\n");
         }
 
+        /// <summary>
+        /// Logs a message in the console window and to the debug window
+        /// </summary>
+        /// <param name="message"></param>
         public static void Log(object message)
         {
             if (!Program.Debug) return;
