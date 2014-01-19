d40127e0e95abf1c7be51354af4625c03a47ad47
helkarakse <nimcuron@gmail.com>
2014-01-19 18:33:26 +0800

    Added new class to contain MojangStatus information

---
+++ Edgebot/Edgebot/MojangStatus.cs
@@ -0,0 +1,16 @@
+﻿namespace Edgebot
+{
+    class MojangStatus
+    {
+        public bool Website { get; set; }
+        public bool Login { get; set; }
+        public bool Session { get; set; }
+        public bool Account { get; set; }
+        public bool Authentication { get; set; }
+
+        public string ToStatusString()
+        {
+            return Website + " " + Login + " " + Session + " " + Account + " " + Authentication;
+        }
+    }
+}
