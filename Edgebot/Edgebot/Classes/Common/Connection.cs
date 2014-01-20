ab4d7b861cd7c627d665b6c65798b643ca126ffe
helkarakse <nimcuron@gmail.com>
2014-01-20 18:59:58 +0800

    Added method to pull web titles via TPL

--- Edgebot/Edgebot/Classes/Common/Connection.cs
+++ Edgebot/Edgebot/Classes/Common/Connection.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.IO;
 using System.Net;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using EdgeBot.Classes.Instances;
 using Newtonsoft.Json.Linq;
@@ -30,6 +31,23 @@ namespace EdgeBot.Classes.Common
         public static void GetServerStatus(Action<MojangStatus> taskSuccess, Action<AggregateException> taskError)
         {
             Task.Factory.StartNew(() => GetMojangStatus())
+                .ContinueWith(t =>
+                {
+                    if (t.Result == null)
+                    {
+                        Utils.Log("Connection: Result is null");
+                    }
+                    else
+                    {
+                        taskSuccess(t.Result);
+                    }
+                })
+                .ContinueWith(t => taskError(t.Exception));
+        }
+
+        public static void GetLinkTitle(string url, Action<string> taskSuccess, Action<AggregateException> taskError)
+        {
+            Task.Factory.StartNew(() => GetWebTitle(url))
                 .ContinueWith(t =>
                 {
                     if (t.Result == null)
@@ -147,6 +165,77 @@ namespace EdgeBot.Classes.Common
             }
 
             return status;
+        }
+
+        public static string GetWebTitle(string url)
+        {
+            var returnString = "";
+            try
+            {
+                Utils.Log("Connection: Getting response from {0}", url);
+                var webRequest = (HttpWebRequest)WebRequest.Create(url);
+                webRequest.Method = "GET";
+                webRequest.ContentType = "application/json";
+                webRequest.KeepAlive = true;
+
+                using (var webResponse = webRequest.GetResponse() as HttpWebResponse)
+                {
+                    if (webResponse != null && webResponse.StatusCode == HttpStatusCode.OK && webResponse.Headers["Content-Type"].StartsWith("text/html"))
+                    {
+                        using (var reader = new StreamReader(webResponse.GetResponseStream()))
+                        {
+                            try
+                            {
+                                var page = reader.ReadToEnd();
+                                var regex = new Regex(@"(?<=<title.*>)([\s\S]*)(?=</title>)", RegexOptions.IgnoreCase);
+                                returnString = regex.Match(page).Value.Trim();
+                            }
+                            catch (Exception)
+                            {
+                                Utils.Log("Connection: Unable to parse stream response: {0}", reader.ReadToEnd());
+                                return null;
+                            }
+                        }
+                    }
+                    else
+                    {
+                        if (webResponse != null)
+                            Utils.Log("Connection: Error fetching data. Server returned status code : {0}",
+                                webResponse.StatusCode);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Utils.Log(ex.StackTrace);
+            }
+
+            return returnString;
+
+            ////var title = GetWebTitle(url);
+            //// Create a request to the url
+            //var webRequest = WebRequest.Create(url) as HttpWebRequest;
+            //// If the request wasn't an HTTP request (like a file), ignore it
+            //if (webRequest == null) return null;
+            //// Use the user's credentials
+            //webRequest.UseDefaultCredentials = true;
+            //// Obtain a response from the server, if there was an error, return nothing
+            //HttpWebResponse response;
+            //try { response = webRequest.GetResponse() as HttpWebResponse; }
+            //catch (WebException) { return null; }
+            //// Regular expression for an HTML title
+            //const string regex = @"(?<=<title.*>)([\s\S]*)(?=</title>)";
+
+            //// If the correct HTML header exists for HTML text, continue
+            //if (response == null || !new List<string>(response.Headers.AllKeys).Contains("Content-Type")) return null;
+            //if (!response.Headers["Content-Type"].StartsWith("text/html")) return null;
+            //// Download the page
+            //var web = new WebClient { UseDefaultCredentials = true };
+            //var page = web.DownloadString(url);
+            //// Extract the title
+            //var ex = new Regex(regex, RegexOptions.IgnoreCase);
+            //return ex.Match(page).Value.Trim();
+            //// Not a valid HTML page
         }
     }
 }
