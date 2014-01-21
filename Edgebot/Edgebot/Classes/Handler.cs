9eca444c2cc6c33706100a21ca9ffe5b3102d431
helkarakse <nimcuron@gmail.com>
2014-01-21 22:24:16 +0800

    Updated update command with version output

--- Edgebot/Edgebot/Classes/Handler.cs
+++ Edgebot/Edgebot/Classes/Handler.cs
@@ -272,6 +272,7 @@ namespace EdgeBot.Classes
         {
             if (paramList.Count < 2)
             {
+                Utils.SendChannel("Current Version: " + Utils.GetVersion("rr", "1"));
                 Utils.SendChannel(Data.RrUpdate);
             }
             else
@@ -279,14 +280,20 @@ namespace EdgeBot.Classes
                 switch (paramList[1])
                 {
                     case "rr":
+                        Utils.SendChannel("Current Version: " + Utils.GetVersion("rr", "1"));
                         Utils.SendChannel(Data.RrUpdate);
                         break;
+
                     case "ftb":
+                        Utils.SendChannel("Current Version: " + Utils.GetVersion("fu", "1"));
                         Utils.SendChannel(Data.FtbUpdate);
                         break;
+
                     case "px":
+                        Utils.SendChannel("Current Version: " + Utils.GetVersion("px", "1"));
                         Utils.SendChannel(Data.PxUpdate);
                         break;
+
                     case "reload":
                         if (Utils.IsOp(nick))
                         {
@@ -297,7 +304,9 @@ namespace EdgeBot.Classes
                             Utils.SendChannel(Data.RestrictedMessage);
                         }
                         break;
+
                     default:
+                        Utils.SendChannel("Current Version: " + Utils.GetVersion("rr", "1"));
                         Utils.SendChannel(Data.RrUpdate);
                         break;
                 }
