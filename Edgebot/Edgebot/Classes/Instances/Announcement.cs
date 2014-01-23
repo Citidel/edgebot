278bd57734e6f68a903a926566a804cd040ea08c
helkarakse <nimcuron@gmail.com>
2014-01-23 12:58:47 +0800

    Created new Announcement class to store Announcement variables

---
+++ Edgebot/Edgebot/Classes/Instances/Announcement.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace EdgeBot.Classes.Instances
+{
+    public static class Announcement
+    {
+        public static Object AnnounceMsg { get; set; }
+        public static Object AnnounceTimes { get; set; }
+    }
+}
