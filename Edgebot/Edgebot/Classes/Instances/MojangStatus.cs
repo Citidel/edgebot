a6f1d6e4919217caf4f8b844dde1ecaa2a6e4c9a
helkarakse <nimcuron@gmail.com>
2014-01-23 12:58:35 +0800

    Removed unused method from MojangStatus.cs

--- Edgebot/Edgebot/Classes/Instances/MojangStatus.cs
+++ Edgebot/Edgebot/Classes/Instances/MojangStatus.cs
@@ -7,10 +7,5 @@
         public bool Session { get; set; }
         public bool Account { get; set; }
         public bool Authentication { get; set; }
-
-        public string ToStatusString()
-        {
-            return Website + " " + Login + " " + Session + " " + Account + " " + Authentication;
-        }
     }
 }
