46bcdfcae04596059bf7845f4f823a247ca20517
helkarakse <nimcuron@gmail.com>
2014-01-26 08:47:28 +0800

    Added abstract class for command handlers

---
+++ Edgebot/Edgebot/Classes/Core/CommandHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using ChatSharp;
+
+namespace EdgeBot.Classes.Core
+{
+    public abstract class CommandHandler
+    {
+        public abstract void HandleCommand(IList<string> paramList, IrcUser user);
+    }
+}
