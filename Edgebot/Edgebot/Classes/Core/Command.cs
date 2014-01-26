32b9daa71f412984384fccb72df8403bf1ca07c6
helkarakse <nimcuron@gmail.com>
2014-01-26 08:47:36 +0800

    Added custom attribute class for Command

---
+++ Edgebot/Edgebot/Classes/Core/Command.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace EdgeBot.Classes.Core
+{
+    public class Command : Attribute
+    {
+        public string Listener { get; set; }
+
+        public Command(string listener)
+        {
+            Listener = listener;
+        }
+    }
+}
