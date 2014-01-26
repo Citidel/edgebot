fad36ca235fbddaf78fb1339a8228da0e58b96e5
helkarakse <nimcuron@gmail.com>
2014-01-26 10:09:16 +0800

    Added isIngameCommand boolean to CommandHandler method signature

--- Edgebot/Edgebot/Classes/Core/CommandHandler.cs
+++ Edgebot/Edgebot/Classes/Core/CommandHandler.cs
@@ -5,6 +5,6 @@ namespace EdgeBot.Classes.Core
 {
     public abstract class CommandHandler
     {
-        public abstract void HandleCommand(IList<string> paramList, IrcUser user);
+        public abstract void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand);
     }
 }
