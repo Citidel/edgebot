6e3d068b6ef14a6d21bcdc9f42cfa7c3ce48ca0c
helkarakse <nimcuron@gmail.com>
2014-01-28 15:28:49 +0800

    Renamed NowPlaying.cs to TeamSpeak.cs

---
+++ Edgebot/Edgebot/Classes/Commands/TeamSpeak.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using ChatSharp;
+using EdgeBot.Classes.Common;
+using EdgeBot.Classes.Core;
+
+namespace EdgeBot.Classes.Commands
+{
+    [Command("ts")]
+    public class TeamSpeak : CommandHandler
+    {
+        public TeamSpeak()
+        {
+        }
+
+        public override void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand)
+        {
+            if (paramList.Count == 1)
+            {
+                Connection.GetTs3(Data.UrlTs3NowPlaying, s =>
+                {
+                    var regex = new Regex(@"^\'*.*\'", RegexOptions.IgnoreCase);
+                    var returnString = regex.Match(s).Value.Trim();
+                    if (string.IsNullOrEmpty(returnString))
+                    {
+                        Utils.SendChannel("No track detected");
+                    }
+                    else
+                    {
+                        Utils.SendChannel("Now playing: " + returnString);
+                    }
+                }, Utils.HandleException);
+            }
+        }
+    }
+}
