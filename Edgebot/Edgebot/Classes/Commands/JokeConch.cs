5dda635a6ec2a5bb8f991656d8bd198cef0e807f
Devihaka <devihaka@gmail.com>
2014-01-31 17:57:34 -0500

    because helk hates me

--- Edgebot/Edgebot/Classes/Commands/JokeConch.cs
+++ Edgebot/Edgebot/Classes/Commands/JokeConch.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using ChatSharp;
 using EdgeBot.Classes.Common;
 using EdgeBot.Classes.Core;
+
 namespace EdgeBot.Classes.Commands
 {
     [Command("conch")]
