8817c3ae8c8cd006173ea3379c999573adda1264
Devihaka <devihaka@gmail.com>
2014-01-31 17:54:49 -0500

    Request fulfillment - !conch

---
+++ Edgebot/Edgebot/Classes/Commands/JokeConch.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChatSharp;
+using EdgeBot.Classes.Common;
+using EdgeBot.Classes.Core;
+namespace EdgeBot.Classes.Commands
+{
+    [Command("conch")]
+    public class JokeConch : CommandHandler
+    {
+        public JokeConch()
+        {
+        }
+
+        public override void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand)
+        {
+            Utils.SendChannel("All hail the magic conch!");
+        }
+    }
+}
