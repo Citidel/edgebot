8f12382ac0a81fa80d8743398aec34a3f452f642
helkarakse <nimcuron@gmail.com>
2014-02-04 22:37:27 +0800

    Removed the need for !info list

--- Edgebot/Edgebot/Classes/Commands/Info.cs
+++ Edgebot/Edgebot/Classes/Commands/Info.cs
@@ -17,42 +17,40 @@ namespace EdgeBot.Classes.Commands
 
         public override void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand)
         {
+            var filter = "";
             if (paramList.Count == 2)
             {
-                var filter = "";
-                if (paramList[1] != "list")
-                {
-                    filter = paramList[1];
-                }
-                var url = !String.IsNullOrEmpty(filter) ? Data.UrlHelp + "/" + filter : Data.UrlHelp + "/all";
-                Connection.GetData(url, "get", jObject =>
-                {
-                    if ((bool) jObject["success"])
-                    {
-                        string outputString;
-                        const string delimiter = ", ";
-                        if (!String.IsNullOrEmpty(filter))
-                        {
-                            var help = JsonConvert.DeserializeObject<JsonHelp>(jObject["result"].ToString());
-                            outputString = help.Value;
-                        }
-                        else
-                        {
-                            outputString =
-                                jObject["result"].Select(row => JsonConvert.DeserializeObject<JsonHelp>(row.ToString()))
-                                    .Aggregate("Keywords: ",
-                                        (current, help) => current + (help.Keyword + delimiter));
-                            outputString = outputString.Substring(0, outputString.Length - delimiter.Length);
-                        }
+                filter = paramList[1];
+            }
 
-                        Utils.SendChannel(outputString);
+            var url = !String.IsNullOrEmpty(filter) ? Data.UrlHelp + "/" + filter : Data.UrlHelp + "/all";
+            Connection.GetData(url, "get", jObject =>
+            {
+                if ((bool)jObject["success"])
+                {
+                    string outputString;
+                    const string delimiter = ", ";
+                    if (!String.IsNullOrEmpty(filter))
+                    {
+                        var help = JsonConvert.DeserializeObject<JsonHelp>(jObject["result"].ToString());
+                        outputString = help.Value;
                     }
                     else
                     {
-                        Utils.SendChannel((string) jObject["message"]);
+                        outputString =
+                            jObject["result"].Select(row => JsonConvert.DeserializeObject<JsonHelp>(row.ToString()))
+                                .Aggregate("Keywords: ",
+                                    (current, help) => current + (help.Keyword + delimiter));
+                        outputString = outputString.Substring(0, outputString.Length - delimiter.Length);
                     }
-                }, Utils.HandleException);
-            }
+
+                    Utils.SendChannel(outputString);
+                }
+                else
+                {
+                    Utils.SendChannel((string)jObject["message"]);
+                }
+            }, Utils.HandleException);
         }
     }
 
