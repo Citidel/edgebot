e1a3af16561a6722cc5ef950d81ddfa0411e5091
helkarakse <nimcuron@gmail.com>
2014-02-04 22:03:45 +0800

    Completed the protected method HandleHelp for CommandHandler

--- Edgebot/Edgebot/Classes/Core/CommandHandler.cs
+++ Edgebot/Edgebot/Classes/Core/CommandHandler.cs
@@ -1,10 +1,28 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using ChatSharp;
+using EdgeBot.Classes.Common;
 
 namespace EdgeBot.Classes.Core
 {
     public abstract class CommandHandler
     {
         public abstract void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand);
+
+        protected static void HandleHelp(IList<string> paramList)
+        {
+            var exists = false;
+            foreach (var help in Program.Commands.Where(cmd => cmd.Value.Listener == paramList[1]).Select(cmd => cmd.Value.Help))
+            {
+                Utils.SendChannel(!string.IsNullOrEmpty(help) ? help : Data.MessageRestricted);
+                exists = true;
+            }
+
+            if (!exists)
+            {
+                Utils.SendChannel("Command not found.");
+            }
+        }
     }
 }
