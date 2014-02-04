6f48cdb42ac3c0a8dfb69cfbd8a4f41848eb75df
helkarakse <nimcuron@gmail.com>
2014-02-04 22:03:56 +0800

    Added method to display help information

---
+++ Edgebot/Edgebot/Classes/Commands/Help.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChatSharp;
+using EdgeBot.Classes.Common;
+using EdgeBot.Classes.Core;
+
+namespace EdgeBot.Classes.Commands
+{
+    [CommandAttribute("help", "")]
+    public class Help : CommandHandler
+    {
+        public Help()
+        {
+        }
+
+        public override void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand)
+        {
+            switch (paramList.Count)
+            {
+                case 1:
+                    var commands = Program.Commands.OrderBy(pair => pair.Value.Listener).Aggregate("", (current, item) => current + (item.Value.Listener + " "));
+                    Utils.SendChannel("Available commands: " + commands.Trim());
+                    break;
+
+                case 2:
+                    HandleHelp(paramList);
+                    break;
+            }
+        }
+    }
+}
