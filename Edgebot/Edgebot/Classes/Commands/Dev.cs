e612f240c7a25883a053e93a716a42e9c1ee72d5
helkarakse <nimcuron@gmail.com>
2014-02-12 13:40:47 +0800

    Added Dev command for testing purposes

---
+++ Edgebot/Edgebot/Classes/Commands/Dev.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ChatSharp;
+using EdgeBot.Classes.Common;
+using EdgeBot.Classes.Core;
+
+namespace EdgeBot.Classes.Commands
+{
+    [CommandAttribute("dev", "")]
+    public class Dev : CommandHandler
+    {
+        public Dev()
+        {
+        }
+
+        public override void HandleCommand(IList<string> paramList, IrcUser user, bool isIngameCommand, string truncatedUser)
+        {
+            if (!Utils.IsDev(user.Nick)) return;
+        }
+    }
+}
